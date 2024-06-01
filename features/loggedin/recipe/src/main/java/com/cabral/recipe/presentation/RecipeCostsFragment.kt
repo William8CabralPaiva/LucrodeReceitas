5a88909e0002f3a4525f62ca6423623dc5c4bd60
“William8Cabral” <suporte@vxm.com.br>
2024-06-01 10:23:24 -0300

    refactor

--- features/loggedin/recipe/src/main/java/com/cabral/recipe/presentation/RecipeCostsFragment.kt
+++ features/loggedin/recipe/src/main/java/com/cabral/recipe/presentation/RecipeCostsFragment.kt
@@ -2,15 +2,11 @@ package com.cabral.recipe.presentation
 
 import android.graphics.Typeface
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.cabral.arch.BaseFragment
 import com.cabral.arch.extensions.removeEndZero
