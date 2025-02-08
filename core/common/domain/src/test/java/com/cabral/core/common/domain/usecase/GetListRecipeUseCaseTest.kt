3850f10e41dca8c908cc32303b121a4f439a0153
“William8Cabral” <suporte@vxm.com.br>
2025-02-08 13:45:47 -0300

    testing module and GetListRecipeUseCaseTest

---
+++ core/common/domain/src/test/java/com/cabral/core/common/domain/usecase/GetListRecipeUseCaseTest.kt
@@ -0,0 +1,53 @@
+package com.cabral.core.common.domain.usecase
+
+import com.cabral.core.common.domain.repository.RecipeRepository
+import com.cabral.stubs.listRecipeProfitPriceStub
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.test.assertFailsWith
+
+
+@ExperimentalCoroutinesApi
+ class GetListRecipeUseCaseTest {
+
+    private val repository: RecipeRepository = mockk()
+    private val subject = GetListRecipeUseCase(repository)
+
+    @Test
+    fun `getAllRecipes Should return recipe list`() = runTest {
+        //Arrange
+        val expectedResult = flowOf(listRecipeProfitPriceStub())
+        coEvery { repository.getAllRecipes() } returns expectedResult
+
+        //Act
+        val result = subject()
+
+        //Assert
+        assertEquals(expectedResult, result)
+        coVerify { repository.getAllRecipes() }
+
+    }
+
+    @Test
+    fun `getAllRecipes Should return recipe failure`() = runTest {
+        //Arrange
+        val expectedResult = "Test error"
+        val throwable = Throwable(expectedResult)
+        coEvery { repository.getAllRecipes() } throws throwable
+
+        //Act
+        val result = assertFailsWith<Throwable> { subject() }
+
+        //Assert
+        assertEquals(expectedResult, result.message)
+        coVerify { repository.getAllRecipes() }
+
+    }
+
+}
