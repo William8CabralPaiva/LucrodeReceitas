483627708b0fdcb5e5db7b5df1276ab0bd82c89b
“William8Cabral” <suporte@vxm.com.br>
2025-03-26 00:11:02 -0300

    remove unused imports

--- core/data/hubSrc/src/main/java/com/cabral/hubsrc/source/remote/services/RecipeServices.kt
+++ core/data/hubSrc/src/main/java/com/cabral/hubsrc/source/remote/services/RecipeServices.kt
@@ -1,18 +1,9 @@
 package com.cabral.hubsrc.source.remote.services
 
-import com.cabral.arch.extensions.GenericThrowable
-import com.cabral.core.common.SingletonUser
 import com.cabral.core.common.domain.model.Recipe
 import com.cabral.core.common.domain.model.RecipeProfitPrice
-import com.cabral.core.common.domain.model.RecipeRegister
-import com.cabral.core.common.domain.model.toListRecipeProfitPrice
-import com.cabral.core.common.domain.model.toRecipe
-import com.cabral.core.common.domain.model.toRecipeRegister
-import com.cabral.hubsrc.source.DBConstants
-import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.tasks.await
 
 interface RecipeServices {
     fun getAllRecipes(): Flow<List<RecipeProfitPrice>> = flow {}
